718d01c9625d5015e832921a609d2fbfae23a598
Luísa Menezes | Lume <luisa_menezes@usp.br>
2024-05-12 18:32:56 -0300

    add update book status function

--- src/main/kotlin/com/booktrack/models/Book.kt
+++ src/main/kotlin/com/booktrack/models/Book.kt
@@ -3,18 +3,28 @@ package com.booktrack.models
 import java.util.concurrent.atomic.AtomicInteger
 
 class Book
+
 private constructor(val id: Int,
                     var title: String,
                     var author: String,
                     var cover: String,
                     var currentPage: Int,
                     var isPage: String,
-                    var finished: Boolean) {
+                    var finished: Boolean,
+                    var messageStatus: String) {
+
+    fun updateStatus(finished: Boolean, currentPage: Int, isPage: String) {
+        if (finished) { this.messageStatus = "Lido" }
+        else { this.messageStatus = isPage + " $currentPage"
+        }
+        this.finished = finished
+    }
+
     companion object {
         private val id = AtomicInteger()
 
-        fun newEntry(title: String, author: String, cover: String, currentPage: Int, isPage: String, finished: Boolean) =
-            Book(id.getAndIncrement(), title, author, cover, currentPage, isPage, finished)
+        fun newEntry(title: String, author: String, cover: String, currentPage: Int, isPage: String, finished: Boolean, messageStatus: String) =
+            Book(id.getAndIncrement(), title, author, cover, currentPage, isPage, finished, messageStatus)
     }
 }
 
@@ -24,5 +34,6 @@ val books = mutableListOf(Book.newEntry(
     "https://m.media-amazon.com/images/I/81C1Y25irCL._AC_UF1000,1000_QL80_.jpg",
     36,
     "Página",
-    false
+    false,
+    "Página 36"
 ))
